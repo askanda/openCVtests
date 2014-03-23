a93e0901523f946a2f62302ded7fd97c9b8d3ab8
Arunan <fletchlab@FletchLabs-MacBook-Pro.local>
2014-03-22 21:25:46 -0700

    Initial Commit

---
+++ openCVtests/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  openCVtests
+//
+//  Created by Arunan on 3/22/14.
+//  Copyright (c) 2014 CellScope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ openCVtests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  openCVtests
+//
+//  Created by Arunan on 3/22/14.
+//  Copyright (c) 2014 CellScope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
